af21613ea28d57206384e26d9bfa5d5b0c0a7fc4
Alabi Abdulmumuni <alabiabdulmumuni9@gmail.com>
2020-12-24 13:06:57 +0100

    update

--- quiz-app/client/js/protected-pages/pages/dashboard/index.js
+++
@@ -1,9 +0,0 @@
-export function dashboard() {
-    document.getElementById('main').innerHTML = `
-        <h1>Hello! Welcome to your Dashboard</h1>
-
-        <h5>Click on the items on the navbar to  begin</h5>
-
-        <button>Click here to start a quiz</button>
-    `;
-}
